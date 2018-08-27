5650ec8e2fe51f6221e1354ad2330d6a2288ac8a
Jaroslaw Nowakowski <nowakowski.priv@gmail.com>
2018-08-27 21:15:00 +0200

    logo added, createElements function: maincontainer, logo

--- static/js/frontpage.js
+++ static/js/frontpage.js
@@ -1,5 +1,20 @@
 let mainDOM = {
     createElements : function(){
-        
+        let mainContainer = document.createElement('div');
+        mainContainer.setAttribute('class', 'maincontainer');
+        mainContainer.setAttribute('id', 'maincontainer');
+        document.body.appendChild(mainContainer);
+        this.createLogo();
+      
+    },
+
+    createLogo : function(){
+        let logo = document.createElement('img');
+        logo.setAttribute('src', '/static/img/brain.jpg');
+        logo.setAttribute('alt', 'brain');
+        logo.setAttribute('id', 'logo');
+        document.getElementById('maincontainer').appendChild(logo);
     }
+
+    
 }
